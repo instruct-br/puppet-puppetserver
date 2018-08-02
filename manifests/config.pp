a4d498a401278fbe2f7f4d244ccb17015ca832a5
Guto Carvalho <gutocarvalho@instruct.com.br>
2018-08-02 14:00:58 -0300

    Add puppetserver ca configs in config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,11 +4,27 @@ class puppetserver::config {
   augeas {'main_certname':
     context => '/files/etc/puppetlabs/puppet/puppet.conf',
     changes => [ "set master/certname ${puppetserver::certname}", ],
+    notify  => Service['puppetserver']
   }
 
   augeas {'main_server':
     context => '/files/etc/puppetlabs/puppet/puppet.conf',
-    changes => [ "set master/server ${puppetserver::certname}", ],
+    changes => [ "set master/server ${puppetserver::main_server}", ],
+    notify  => Service['puppetserver']
+  }
+
+  augeas {'ca_server':
+      context => '/files/etc/puppetlabs/puppet/puppet.conf',
+      changes => [ "set main/ca_server ${puppetserver::ca_server}", ],
+      notify  => Service['puppetserver']
+  }
+
+  if $puppetserver::enable_ca {
+    augeas {'dns_alt_names':
+      context => '/files/etc/puppetlabs/puppet/puppet.conf',
+      changes => [ "set main/dns_alt_names ${puppetserver::dns_alt_names}", ],
+      notify  => Service['puppetserver']
+    }
   }
 
   augeas {'java_args':
@@ -17,12 +33,17 @@ class puppetserver::config {
     notify  => Service['puppetserver']
   }
 
+  file { 'ca_config':
+    ensure  => file,
+    path    => '/etc/puppetlabs/puppetserver/services.d/ca.cfg',
+    content => epp('puppetserver/ca.cfg.epp'),
+    notify  => Service['puppetserver']
+  }
+
   if $puppetserver::autosign {
     augeas {'master_autosign':
       context => '/files/etc/puppetlabs/puppet/puppet.conf/master',
-      changes => [
-        'set autosign true',
-      ],
+      changes => [ 'set autosign true', ],
       notify  => Service['puppetserver']
     }
   }
