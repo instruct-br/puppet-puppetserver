8baf356437d3689959184da89da1791aa3136cae
Guto Carvalho <gutocarvalho@instruct.com.br>
2018-08-02 14:05:42 -0300

    Added two new smoke tests and remove old test

---
+++ examples/master-compiler.pp
@@ -0,0 +1,9 @@
+class { 'puppetserver':
+  version      => '5.3.4-1.el7',
+  autosign     => true,
+  enable_ca    => false,
+  certname     => $trusted['certname'],
+  ca_server    => 'masterca.puppet.dev',
+  dns_altnames => [ 'puppet', 'puppetmaster' ]
+  java_args    => '-Xms2g -Xmx2g -XX:MaxPermSize=256m',
+}
