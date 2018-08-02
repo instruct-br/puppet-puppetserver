36f9ad5b5879a501efa6850306638957d63d2997
Taciano Tres <tacianotres@gmail.com>
2018-08-02 15:07:21 -0300

    Fix syntax typo

--- examples/master-compiler.pp
+++ examples/master-compiler.pp
@@ -1,9 +1,9 @@
 class { 'puppetserver':
   version      => '5.3.4-1.el7',
-  autosign     => true,
+  autosign     => false,
   enable_ca    => false,
   certname     => $trusted['certname'],
   ca_server    => 'masterca.puppet.dev',
-  dns_altnames => [ 'puppet', 'puppetmaster' ]
+  dns_altnames => [ 'puppet', 'puppetmaster' ],
   java_args    => '-Xms2g -Xmx2g -XX:MaxPermSize=256m',
 }
